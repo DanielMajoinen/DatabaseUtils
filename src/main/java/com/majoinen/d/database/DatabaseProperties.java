7fff87e92cc1eac67595148ce62679ac40c26cf2
Daniel Majoinen <daniel@majoinen.com>
2017-07-05 21:04:33 +1000

    Add DatabaseProperties interface

---
+++ src/main/java/com/majoinen/d/database/DatabaseProperties.java
@@ -0,0 +1,15 @@
+package com.majoinen.d.database;
+
+import java.util.List;
+
+/**
+ * Getters for properties required no matter the database implementation type,
+ * accessed from its corresponding config file.
+ *
+ * @author Daniel Majoinen
+ * @version 1.0, 5/7/17
+ */
+public interface DatabaseProperties {
+    String getDatabaseName();
+    List<String> getTableNames();
+}
