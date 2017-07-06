fa4b3c162501ced864d37a2e915c9cc8c63640c4
Daniel Majoinen <daniel@majoinen.com>
2017-07-06 11:54:07 +1000

    Add DatabaseType enum for all supported database implementations

---
+++ src/main/java/com/majoinen/d/database/DatabaseType.java
@@ -0,0 +1,11 @@
+package com.majoinen.d.database;
+
+/**
+ * Enum for all database types supported by this library.
+ *
+ * @author Daniel Majoinen
+ * @version 1.0, 5/7/17
+ */
+public enum DatabaseType {
+    SQLITE
+}
