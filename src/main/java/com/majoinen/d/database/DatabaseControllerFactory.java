c7c89a5ac686e685a6561d37a3bb6254762c3ee1
Daniel Majoinen <daniel@majoinen.com>
2017-07-06 12:00:30 +1000

    Add DatabaseControllerFactory singleton

---
+++ src/main/java/com/majoinen/d/database/DatabaseControllerFactory.java
@@ -0,0 +1,28 @@
+package com.majoinen.d.database;
+
+import com.majoinen.d.database.sqlite.SQLiteDatabaseController;
+
+/**
+ * A factory to create an appropriate database controller for the desired
+ * database implementation type.
+ *
+ * @author Daniel Majoinen
+ * @version 1.0, 5/7/17
+ */
+public class DatabaseControllerFactory {
+
+    private static DatabaseControllerFactory instance = null;
+
+    private static DatabaseControllerFactory getInstance() {
+        if(instance == null)
+            instance = new DatabaseControllerFactory();
+        return instance;
+    }
+
+    public DatabaseController getController(DatabaseType type) {
+        DatabaseController controller = null;
+        if(type.equals(DatabaseType.SQLITE))
+            controller = new SQLiteDatabaseController();
+        return controller;
+    }
+}
