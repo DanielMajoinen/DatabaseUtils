cad2b5087df48b43c48523d80a56f6c49fc7be2b
Daniel Majoinen <daniel@majoinen.com>
2017-07-07 00:54:35 +1000

    Rename database.properties to dbutils.properties

--- src/main/java/com/majoinen/d/database/AbstractDatabaseProperties.java
+++ src/main/java/com/majoinen/d/database/AbstractDatabaseProperties.java
@@ -13,7 +13,7 @@ public abstract class AbstractDatabaseProperties implements DatabaseProperties {
     protected static final String CONFIG_RESOURCE_DIR = "/config/";
 
     // The name of the config file
-    protected static final String CONFIG_FILENAME = "database.properties";
+    protected static final String CONFIG_FILENAME = "dbutils.properties";
 
     // The key for the database name in the config .properties file
     protected static final String DATABASE_NAME_KEY = "databaseName";
@@ -21,6 +21,6 @@ public abstract class AbstractDatabaseProperties implements DatabaseProperties {
     // The key for the table names list in the config .properties file
     protected static final String TABLE_NAMES_KEY = "tableNames";
 
-    // The key for the tables delimiter in database.properties
+    // The key for the tables delimiter in dbutils.properties
     protected static final String TABLES_DELIMITER_KEY = "delimiter";
 }
