c5eb517d951c71be6611346d7836ac7df8c3b2fa
Daniel Majoinen <daniel@majoinen.com>
2017-07-07 00:55:17 +1000

    Pass calling class on init instead of getInstance

--- src/main/java/com/majoinen/d/database/DatabaseInitialiser.java
+++ src/main/java/com/majoinen/d/database/DatabaseInitialiser.java
@@ -1,5 +1,8 @@
 package com.majoinen.d.database;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 /**
  * The DatabaseInitialiser is responsible for creating/initialising the
  * database and potentially validating all tables.
@@ -8,5 +11,15 @@ package com.majoinen.d.database;
  * @version 1.0, 5/7/17
  */
 public interface DatabaseInitialiser {
-    void init();
+
+    /**
+     * Verify the database. If the database does not exist, create it. If the
+     * database fails verification for any reason, back up the database and
+     * create a new database.
+     * @throws SQLException If there was a database error when verifying a
+     * table or connecting to the database.
+     * @throws IOException If the database config file is not found or if there
+     * are any permission issues when accessing the config file.
+     */
+    void init(Class<?> caller) throws SQLException, IOException;
 }
