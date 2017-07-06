ee26c6507f760a305b8bd2ce186523b3accecee9
Daniel Majoinen <daniel@majoinen.com>
2017-07-06 11:56:32 +1000

    Add DatabaseInitialiser interface

---
+++ src/main/java/com/majoinen/d/database/DatabaseInitialiser.java
@@ -0,0 +1,12 @@
+package com.majoinen.d.database;
+
+/**
+ * The DatabaseInitialiser is responsible for creating/initialising the
+ * database and potentially validating all tables.
+ *
+ * @author Daniel Majoinen
+ * @version 1.0, 5/7/17
+ */
+public interface DatabaseInitialiser {
+    void init();
+}
