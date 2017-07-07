09cc97c2da1177a55fde252706b9b0345dde67a8
Daniel Majoinen <daniel@majoinen.com>
2017-07-07 18:23:19 +1000

    Modify to throw exceptions when instantiating controller

--- src/main/java/com/majoinen/d/database/DatabaseControllerFactory.java
+++ src/main/java/com/majoinen/d/database/DatabaseControllerFactory.java
@@ -2,6 +2,9 @@ package com.majoinen.d.database;
 
 import com.majoinen.d.database.sqlite.SQLiteDatabaseController;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 /**
  * A factory to create an appropriate database controller for the desired
  * database implementation type.
@@ -11,7 +14,7 @@ import com.majoinen.d.database.sqlite.SQLiteDatabaseController;
  */
 public class DatabaseControllerFactory {
     public static DatabaseController getController(DatabaseType type,
-      Class<?> caller) {
+      Class<?> caller) throws SQLException, IOException {
         DatabaseController controller = null;
         if(type.equals(DatabaseType.SQLITE))
             controller = new SQLiteDatabaseController(caller);
