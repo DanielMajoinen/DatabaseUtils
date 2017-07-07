a58e0c3448c4b47a2362e7fa7dbf5d7f1f45dc3e
Daniel Majoinen <daniel@majoinen.com>
2017-07-08 01:16:13 +1000

    Modify to extend DBUtilsException

--- src/main/java/com/majoinen/d/database/exception/TableMismatchException.java
+++ src/main/java/com/majoinen/d/database/exception/TableMismatchException.java
@@ -1,7 +1,5 @@
 package com.majoinen.d.database.exception;
 
-import java.sql.SQLException;
-
 /**
  * A TableMismatchException occurs when a table is not as expected while
  * verifying a database.
@@ -9,7 +7,7 @@ import java.sql.SQLException;
  * @author Daniel Majoinen
  * @version 1.0, 5/7/17
  */
-public class TableMismatchException extends SQLException {
+public class TableMismatchException extends DBUtilsException {
     public TableMismatchException() {}
 
     public TableMismatchException(String message) {
