d685db79820e0c9deea4588d56c1a0bf7415a608
Daniel Majoinen <daniel@majoinen.com>
2017-07-07 15:37:20 +1000

    Make getController static and remove factory instance

--- src/main/java/com/majoinen/d/database/DatabaseControllerFactory.java
+++ src/main/java/com/majoinen/d/database/DatabaseControllerFactory.java
@@ -10,17 +10,8 @@ import com.majoinen.d.database.sqlite.SQLiteDatabaseController;
  * @version 1.0, 5/7/17
  */
 public class DatabaseControllerFactory {
-
-    private static DatabaseControllerFactory instance = null;
-
-    public static DatabaseControllerFactory getInstance() {
-        if(instance == null)
-            instance = new DatabaseControllerFactory();
-        return instance;
-    }
-
-    public DatabaseController getController(DatabaseType type, Class<?>
-      caller) {
+    public static DatabaseController getController(DatabaseType type,
+      Class<?> caller) {
         DatabaseController controller = null;
         if(type.equals(DatabaseType.SQLITE))
             controller = new SQLiteDatabaseController(caller);
