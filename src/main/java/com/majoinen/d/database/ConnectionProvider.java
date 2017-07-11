536184194afc91c8912d333950be7f5ef1a9b83f
Daniel Majoinen <daniel@majoinen.com>
2017-07-11 14:21:56 +1000

    Add ConnectionProvider interface

---
+++ src/main/java/com/majoinen/d/database/ConnectionProvider.java
@@ -0,0 +1,15 @@
+package com.majoinen.d.database;
+
+import com.majoinen.d.database.exception.DBUtilsException;
+
+import java.sql.Connection;
+
+/**
+ * Defines a class which can provide a connection for any database types.
+ *
+ * @author Daniel Majoinen
+ * @version 1.0, 11/7/17
+ */
+public interface ConnectionProvider {
+    Connection openConnection() throws DBUtilsException;
+}
