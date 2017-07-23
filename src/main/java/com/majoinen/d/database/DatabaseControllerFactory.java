b412350e7ff46165412c8a3c5a1d23a17ce0e467
Daniel Majoinen <daniel@majoinen.com>
2017-07-23 21:24:16 +1000

    Cache instantiated database controllers

--- src/main/java/com/majoinen/d/database/DatabaseControllerFactory.java
+++ src/main/java/com/majoinen/d/database/DatabaseControllerFactory.java
@@ -3,6 +3,9 @@ package com.majoinen.d.database;
 import com.majoinen.d.database.exception.DBUtilsException;
 import com.majoinen.d.database.sqlite.SQLiteDatabaseController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A factory to create an appropriate database controller for the desired
  * database implementation type.
@@ -12,13 +15,23 @@ import com.majoinen.d.database.sqlite.SQLiteDatabaseController;
  */
 public final class DatabaseControllerFactory {
 
+    private static Map<String, DatabaseController> controllers;
+
     private DatabaseControllerFactory() { }
 
-    public static DatabaseController getController(DatabaseType type, String
-      databaseName) throws DBUtilsException {
+    public static DatabaseController getController(String databaseName)
+      throws DBUtilsException {
+        if(controllers == null)
+            controllers = new HashMap<>();
+        else if(controllers.containsKey(databaseName))
+            return controllers.get(databaseName);
+
         DatabaseController controller = null;
+        DatabaseType type = DatabaseProperties.getDatabaseType(databaseName);
         if(type.equals(DatabaseType.SQLITE))
             controller = new SQLiteDatabaseController(databaseName);
+
+        controllers.put(databaseName, controller);
         return controller;
     }
 }
