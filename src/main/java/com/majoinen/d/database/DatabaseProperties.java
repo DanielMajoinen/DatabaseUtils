d1b018bf986863d9abb3e3da1e3c5c52afd2accd
Daniel Majoinen <daniel@majoinen.com>
2017-07-23 21:23:13 +1000

    Abstract shared database properties into DatabaseProperties

--- src/main/java/com/majoinen/d/database/DatabaseProperties.java
+++ src/main/java/com/majoinen/d/database/DatabaseProperties.java
@@ -1,17 +1,60 @@
 package com.majoinen.d.database;
 
 import com.majoinen.d.database.exception.DBUtilsException;
+import com.majoinen.d.database.util.PropertiesHandler;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
- * Getters for properties required no matter the database implementation type,
- * defined in its corresponding config file.
+ * Abstract properties class which provides required keys and parameters for
+ * any implementation type.
  *
  * @author Daniel Majoinen
- * @version 1.0, 5/7/17
+ * @version 1.0, 6/7/17
  */
-public interface DatabaseProperties {
-    String getDatabaseName() throws DBUtilsException;
-    List<String> getTableNames() throws DBUtilsException;
+public abstract class DatabaseProperties {
+
+    // The key for the database type
+    private static final String DATABASE_TYPE_KEY = "database.type";
+
+    // The key for the table names list in the config .properties file
+    private static final String TABLE_NAMES_KEY = "table.names";
+
+    // The key for the tables delimiter
+    private static final String TABLES_DELIMITER_KEY = "table.names.delimiter";
+
+    protected DatabaseProperties() {}
+
+    /**
+     * Get the database type defined in the database .properties file.
+     *
+     * @param databaseName the database to get the type of.
+     * @return the database type as a DatabaseType enum.
+     * @throws DBUtilsException
+     */
+    public static DatabaseType getDatabaseType(String databaseName)
+      throws DBUtilsException {
+        String databaseType =
+          PropertiesHandler.getProperty(databaseName, DATABASE_TYPE_KEY);
+        if("SQLITE".equalsIgnoreCase(databaseType))
+            return DatabaseType.SQLITE;
+        throw new NullPointerException("[DBUtils] Database type unknown");
+    }
+
+    /**
+     * Get a list of table names as specified in the appropriate config file.
+     *
+     * @return A list of table names in/or meant to be in the database.
+     * @throws DBUtilsException If the config file is not found or if there are
+     * any permission issues when accessing the config file.
+     */
+    public static List<String> getTableNames(String databaseName)
+      throws DBUtilsException {
+        String delimiter = PropertiesHandler
+          .getProperty(databaseName, TABLES_DELIMITER_KEY);
+        String tables = PropertiesHandler
+          .getProperty(databaseName, TABLE_NAMES_KEY);
+        return Arrays.asList(tables.split(delimiter));
+    }
 }
